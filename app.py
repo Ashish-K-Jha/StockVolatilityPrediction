08c91e6f7b276a6476337d325f5de7c87bf9c16d
Ashish-K-Jha <119500129+Ashish-K-Jha@users.noreply.github.com>
2025-04-04 20:48:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ app = Flask(__name__)
 
 # Initialize model
 model = VolatilityModel()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
 
 # Check if model exists, if not, train it
 if not os.path.exists('volatility_model.pkl'):
